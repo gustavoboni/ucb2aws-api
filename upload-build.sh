76706036549c0a8b29b541a0f0c6de4f1c2f5117
Gustavo Boni <gustavo@shoelacelearning.com>
2023-03-22 17:05:57 -0400

    removing profile from aws cli commnands

--- upload-build.sh
+++ upload-build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo -----------------------------------------
-echo ----- Welcome to the build uploader -----
-echo ------- Written by Gustavo Boni ---------
+echo -------- Shoelace build uploader --------
+echo ------------ by Gustavo Boni ------------
 echo -----------------------------------------
 echo Build version is $1
 echo Build path is $2
 echo Build Config name is $3
 echo S3 bucket path is $4
 
-aws s3 cp $2\ $4/$1/ --recursive --profile prod --exclude "Build/*"
-aws s3 cp $2\Build\$3.data.gz $4/$1/Build/ --content-encoding gzip --profile prod
-aws s3 cp $2\Build\$3.framework.js.gz $4/$1/Build/ --content-encoding gzip --content-type application/javascript --profile prod
-aws s3 cp $2\Build\$3.wasm.gz $4/$1/Build/ --content-encoding gzip --profile prod
-aws s3 cp $2\Build\$3.loader.js $4/$1/Build/ --content-type txt/javascript --profile prod
-aws cloudfront create-invalidation --distribution-id E14TGSHQ490P7P --paths "/internal/$1/*" --profile prod
+aws s3 cp $2/ $4/$1/ --recursive --exclude "Build/*"
+aws s3 cp $2/Build/$3.data.gz $4/$1/Build/ --content-encoding gzip
+aws s3 cp $2/Build/$3.framework.js.gz $4/$1/Build/ --content-encoding gzip --content-type application/javascript
+aws s3 cp $2/Build/$3.wasm.gz $4/$1/Build/ --content-encoding gzip
+aws s3 cp $2/Build/$3.loader.js $4/$1/Build/ --content-type txt/javascript
+aws cloudfront create-invalidation --distribution-id E14TGSHQ490P7P --paths "/internal/$1/*"
